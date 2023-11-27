916ed625af455d8b318f80057516b6914fd96575
Antoine Dangeard <antoine.dangeard@gmail.com>
2023-11-27 16:57:54 -0500

    added datasets

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,2 +1,11 @@
 class Dataset:
-    def __init__(self, )
+    def __init__(self, path_to_folder):
+        self.path_to_folder = path_to_folder
+        isClassification
+        inputSize
+        outputSize
+        train_input
+        train_output
+        test_input
+        test_output
+        outputSize
