0e1549c30458f0b329bc521ddfe00f442bfb9dce
Antoine Dangeard <antoine.dangeard@gmail.com>
2023-11-29 00:26:38 -0500

    add experiments 4 and 5

--- experiments.py
+++ experiments.py
@@ -4,9 +4,9 @@ from learn import learn
 # 2 - Mutation probability
 # 3 - Trade off Population size vs number of evolution steps
 # 4 - Elitism proportion
-# 5 - Different crossover functions
-# 6 - Random baseline + model baseline
-# 7 - Fitness function weights (convergence speed vs performance)
+# 5 - Fitness function weights (convergence speed vs performance)
+
+# 7 - our algorithm vs. hyperparameter grid search baseline (number of epochs until a certain loss is found)
 # 8 - Plot fitness for specific hyper params (genes)
 
 # Genome templates
@@ -21,6 +21,16 @@ def experiment_1():
     learn(genome_type=0, dataset_type="diabetes", experiment_name="gen_1_diabetes")
     learn(genome_type=1, dataset_type="diabetes", experiment_name="gen_2_diabetes")
     learn(genome_type=2, dataset_type="diabetes", experiment_name="gen_3_diabetes")
+    
+    #THIRD DATASET
+    learn(genome_type=0, dataset_type="hardness", experiment_name="gen_0_hardness")
+    learn(genome_type=1, dataset_type="hardness", experiment_name="gen_1_hardness")
+    learn(genome_type=2, dataset_type="hardness", experiment_name="gen_2_hardness")
+
+    #FOURTH DATASET
+    learn(genome_type=0, dataset_type="credit", experiment_name="gen_1_credit")
+    learn(genome_type=1, dataset_type="credit", experiment_name="gen_2_credit")
+    learn(genome_type=2, dataset_type="credit", experiment_name="gen_3_credit")
 
 
 # Mutation probability
@@ -35,6 +45,16 @@ def experiment_2():
     learn(mutation_prob=10, dataset_type="diabetes", experiment_name="mut_10_diabetes")
     learn(mutation_prob=20, dataset_type="diabetes", experiment_name="mut_20_diabetes")
     learn(mutation_prob=30, dataset_type="diabetes", experiment_name="mut_30_diabetes")
+    
+    #THIRD DATASET
+    learn(mutation_prob=10, dataset_type="hardness", experiment_name="mut_10_hardness")
+    learn(mutation_prob=20, dataset_type="hardness", experiment_name="mut_20_hardness")
+    learn(mutation_prob=30, dataset_type="hardness", experiment_name="mut_30_hardness")
+
+    #FOURTH DATASET
+    learn(mutation_prob=10, dataset_type="credit", experiment_name="mut_10_credit")
+    learn(mutation_prob=20, dataset_type="credit", experiment_name="mut_20_credit")
+    learn(mutation_prob=30, dataset_type="credit", experiment_name="mut_30_credit")
 
 
 # Trade off Population size vs number of evolution steps
@@ -50,9 +70,69 @@ def experiment_3():
     learn(populationSize=50, max_evolutionary_steps=20,  dataset_type="housing", experiment_name="avgpop_avgsteps_diabetes") # average pop size, average max evolutionary steps 
     learn(populationSize=10, max_evolutionary_steps=100, dataset_type="housing", experiment_name="lowpop_highsteps_diabetes") # low pop size, high max evolutionary steps 
 
+    #THIRD DATASET
+    learn(populationSize=200, max_evolutionary_steps=5, dataset_type="hardness", experiment_name="highpop_lowsteps_hardness") # high pop size, low max evolutionary steps 
+    learn(populationSize=50, max_evolutionary_steps=20,  dataset_type="hardness", experiment_name="avgpop_avgsteps_hardness") # average pop size, average max evolutionary steps 
+    learn(populationSize=10, max_evolutionary_steps=100, dataset_type="hardness", experiment_name="lowpop_highsteps_hardness") # low pop size, high max evolutionary steps 
 
+    #FOURTH DATASET
+    learn(populationSize=200, max_evolutionary_steps=5, dataset_type="credit", experiment_name="highpop_lowsteps_credit") # high pop size, low max evolutionary steps 
+    learn(populationSize=50, max_evolutionary_steps=20,  dataset_type="credit", experiment_name="avgpop_avgsteps_credit") # average pop size, average max evolutionary steps 
+    learn(populationSize=10, max_evolutionary_steps=100, dataset_type="credit", experiment_name="lowpop_highsteps_credit") # low pop size, high max evolutionary steps 
+
+# Elitism proportion
+def experiment_4():
+
+    #FIRST DATASET
+    learn(dataset_type="housing", experiment_name="no_elitism_housing", populationElitismProportion = 0.0) # high pop size, low max evolutionary steps 
+    learn(dataset_type="housing", experiment_name="avg_elitism_housing", populationElitismProportion = 0.2) # high pop size, low max evolutionary steps 
+    learn(dataset_type="housing", experiment_name="high_elitism_housing", populationElitismProportion = 0.5) # high pop size, low max evolutionary steps 
+
+    #SECOND DATASET
+    learn(dataset_type="diabetes", experiment_name="no_elitism_diabetes", populationElitismProportion = 0.0) # high pop size, low max evolutionary steps 
+    learn(dataset_type="diabetes", experiment_name="avg_elitism_diabetes", populationElitismProportion = 0.2) # high pop size, low max evolutionary steps 
+    learn(dataset_type="diabetes", experiment_name="high_elitism_diabetes", populationElitismProportion = 0.5) # high pop size, low max evolutionary steps 
+    
+    #THIRD DATASET
+    learn(dataset_type="hardness", experiment_name="no_elitism_hardness", populationElitismProportion = 0.0) # high pop size, low max evolutionary steps 
+    learn(dataset_type="hardness", experiment_name="avg_elitism_hardness", populationElitismProportion = 0.2) # high pop size, low max evolutionary steps 
+    learn(dataset_type="hardness", experiment_name="high_elitism_hardness", populationElitismProportion = 0.5) # high pop size, low max evolutionary steps 
+
+    #FOURTH DATASET
+    learn(dataset_type="credit", experiment_name="no_elitism_credit", populationElitismProportion = 0.0) # high pop size, low max evolutionary steps 
+    learn(dataset_type="credit", experiment_name="avg_elitism_credit", populationElitismProportion = 0.2) # high pop size, low max evolutionary steps 
+    learn(dataset_type="credit", experiment_name="high_elitism_credit", populationElitismProportion = 0.5) # high pop size, low max evolutionary steps 
+
+def experiment_5():
+
+    #FIRST DATASET
+    learn(dataset_type="housing", experiment_name="no_convergence_time_fitness_housing", fitness_loss_weight = 1, fitness_epoch_count_weight = 0) # high pop size, low max evolutionary steps 
+    learn(dataset_type="housing", experiment_name="some_convergence_and_loss_fitness_housing", fitness_loss_weight = 1, fitness_epoch_count_weight = 1/100) # high pop size, low max evolutionary steps 
+    learn(dataset_type="housing", experiment_name="convergence_and_loss_fitness_housing", fitness_loss_weight = 1, fitness_epoch_count_weight = 1/50) # high pop size, low max evolutionary steps 
+    learn(dataset_type="housing", experiment_name="mostly_convergence_time_fitness_housing", fitness_loss_weight = 1, fitness_epoch_count_weight = 1/10) # high pop size, low max evolutionary steps 
+
+    #SECOND DATASET
+    learn(dataset_type="diabetes", experiment_name="no_convergence_time_fitness_diabetes", fitness_loss_weight = 1, fitness_epoch_count_weight = 0) # high pop size, low max evolutionary steps 
+    learn(dataset_type="diabetes", experiment_name="some_convergence_and_loss_fitness_diabetes", fitness_loss_weight = 1, fitness_epoch_count_weight = 1/100) # high pop size, low max evolutionary steps 
+    learn(dataset_type="diabetes", experiment_name="convergence_and_loss_fitness_diabetes", fitness_loss_weight = 1, fitness_epoch_count_weight = 1/50) # high pop size, low max evolutionary steps 
+    learn(dataset_type="diabetes", experiment_name="mostly_convergence_time_fitness_diabetes", fitness_loss_weight = 1, fitness_epoch_count_weight = 1/10) # high pop size, low max evolutionary steps 
+
+    #THIRD DATASET
+    learn(dataset_type="hardness", experiment_name="no_convergence_time_fitness_hardness", fitness_loss_weight = 1, fitness_epoch_count_weight = 0) # high pop size, low max evolutionary steps 
+    learn(dataset_type="hardness", experiment_name="some_convergence_and_loss_fitness_hardness", fitness_loss_weight = 1, fitness_epoch_count_weight = 1/100) # high pop size, low max evolutionary steps 
+    learn(dataset_type="hardness", experiment_name="convergence_and_loss_fitness_hardness", fitness_loss_weight = 1, fitness_epoch_count_weight = 1/50) # high pop size, low max evolutionary steps 
+    learn(dataset_type="hardness", experiment_name="mostly_convergence_time_fitness_hardness", fitness_loss_weight = 1, fitness_epoch_count_weight = 1/10) # high pop size, low max evolutionary steps 
+    
+    #FOURTH DATASET
+    learn(dataset_type="credit", experiment_name="no_convergence_time_fitness_credit", fitness_loss_weight = 1, fitness_epoch_count_weight = 0) # high pop size, low max evolutionary steps 
+    learn(dataset_type="credit", experiment_name="some_convergence_and_loss_fitness_credit", fitness_loss_weight = 1, fitness_epoch_count_weight = 1/100) # high pop size, low max evolutionary steps 
+    learn(dataset_type="credit", experiment_name="convergence_and_loss_fitness_credit", fitness_loss_weight = 1, fitness_epoch_count_weight = 1/50) # high pop size, low max evolutionary steps 
+    learn(dataset_type="credit", experiment_name="mostly_convergence_time_fitness_credit", fitness_loss_weight = 1, fitness_epoch_count_weight = 1/10) # high pop size, low max evolutionary steps 
+    
 if __name__ == "__main__":
 
     experiment_1()
     experiment_2()
     experiment_3()
+    experiment_4()
+    experiment_5()
