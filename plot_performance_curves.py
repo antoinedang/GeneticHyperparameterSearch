31705eb7da999cf1881e40881de454fbaf720c15
Antoine Dangeard <antoine.dangeard@gmail.com>
2023-11-29 19:10:21 -0500

    added gifs for elitism visualization

--- plot_performance_curves.py
+++ plot_performance_curves.py
@@ -8,7 +8,7 @@ def fit_polynomial(x, y, deg):
     polynomial = np.poly1d(coefficients)
     return polynomial
 
-plots = [('gen_0_housing.csv', "Gen 0"), ('gen_1_housing.csv', "Gen 1"), ('gen_2_housing.csv', "Gen 2")]  # Replace with your actual filenames
+plots = [('4_no_elitism_diabetes.csv', "Elitism 0%"), ('4_avg_elitism_diabetes.csv', "Elitism 20%"), ('4_high_elitism_diabetes.csv', "Elitism 50%")]  # Replace with your actual filenames
 x_index = 0
 y_index = 1
 trend_poly_degree = 2
@@ -22,7 +22,7 @@ for filename, line_label in plots:
 
 plt.xlabel('Evolutionary Steps')
 plt.ylabel('Optimal Model Performance')
-plt.title('GA Performance vs. Genome Type')
+plt.title('GA Performance vs. Genome Type (Diabetes)')
 plt.legend()
 plt.grid(True)
 plt.show()
--- visualize_population_evolution.py
+++ visualize_population_evolution.py
@@ -16,14 +16,14 @@ def generateFrame(i):
     plt.ylabel(yAxisName)
     plt.title(plotTitle + ' Iteration {}'.format(epoch_number))
 
-num_bins = 100
+num_bins = 30
 max_val = 1.0
-data_file = 'experiment_results/normal_loss_distribution.csv'  # Replace with your actual filenames
-plotTitle = "Population Distribution"
-xAxisName = "Loss"
+data_file = 'experiment_results/4_high_elitism_diabetes_loss_distribution.csv'  # Replace with your actual filenames
+plotTitle = "Population Distribution with Elitism 50% (Diabetes)"
+xAxisName = "Performance"
 yAxisName = "Frequency"
-videoName = 'experiment_results/population_evolution'
-fps = 2
+videoName = 'experiment_results/population_evolution_diabetes_high_elitism'
+fps = 1
 
 with open(data_file, "r") as f:
     lines = f.readlines()[2:]
